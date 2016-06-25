fa1326b5b47679cd7dbea2122f18f7807ff29f4a
kalolo <kalolo@gmail.com>
2016-06-24 17:22:49 -0700

    Removing files

--- src/main/java/com/ns/mytravel/Application.java
+++ src/main/java/com/ns/mytravel/Application.java
@@ -1,22 +1,11 @@
 package com.ns.mytravel;
 
-import com.ns.mytravel.tasks.InitAirportCodesTask;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.*;
-
 
 
 @SpringBootApplication
 public class Application {
-
-    @Autowired
-    InitAirportCodesTask airportChecks;
-
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
